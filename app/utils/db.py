7eb3ebd0561e72b0b1f69762e73c8e3bcbfb1f00
marisiman <email_dari_marisiman@example.com>
2024-06-03 08:36:54 +0700

    configurated connection db used aiven with mysql workbench

--- app/utils/db.py
+++ app/utils/db.py
@@ -1,14 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
+import pymysql
 from sqlalchemy import create_engine
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import scoped_session, sessionmaker
 import os
 
+pymysql.install_as_MySQLdb()
+
 # Creating a SQLAlchemy db object without direct initialization
 db = SQLAlchemy()
 
 DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USERNAME")
+DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
